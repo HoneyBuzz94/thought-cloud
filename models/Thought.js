35bd18302adf413d884a071d0b3c6e63ec14b791
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2024-02-28 14:46:10 -0700

    Updated Thought and User models to match challenge requirements

--- models/Thought.js
+++ models/Thought.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 const Reaction = require('./Reaction');
 
 const thoughtSchema = new mongoose.Schema({
-    text: { type: String, required: true },
+    text: { type: String, required: true, minLength: 1, maxLength: 280 },
     createdAt: { type: Date, default: Date.now },
+    username: { type: String, required: true },
     reactions: [Reaction],
 });
+
+thoughtSchema.virtual('reactionCount').get(function() { this.reactions.length });
 
 const Thought = mongoose.model('thought', thoughtSchema);
 
--- models/User.js
+++ models/User.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true },
+    username: { type: String, unique: true, required: true, trim: true },
+    email: { type: String, unique: true, required: true, validate: { validator: () => Promise.resolve(false), message: 'Email validation failed' } },
     password: { type: String, required: true },
-    thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'thought' }]
+    thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'thought' }],
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: this }]
 });
+
+userSchema.virtual('friendCount').get(function() { return this.friends.length });
 
 const User = mongoose.model('user', userSchema);
 
